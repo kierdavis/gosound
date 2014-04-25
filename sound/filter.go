95b0035d571600eeda344115407705f44ebb49af
Kier Davis <kierdavis@gmail.com>
2014-04-25 20:07:46 +0100

    Add cutoff frequency-to-alpha conversion for high/low-pass filters

--- sound/filter.go
+++ sound/filter.go
@@ -1,5 +1,34 @@
 package sound
 
+import (
+	"math"
+)
+
+// Convert a high pass filter's cutoff frequency to an alpha value
+func (ctx Context) HighPassCutoffToAlpha(freq float64) (alpha float64) {
+	// Time interval between samples
+	dt := 1 / ctx.SampleRate
+	
+	// Time constant (of analogue circuit)
+	rc := 1 / (2 * math.Pi * freq)
+	
+	return rc / (rc + dt)
+}
+
+func (ctx Context) HighPassCutoffToAlphaM(freqs chan float64) (alphas chan float64) {
+	alphas = make(chan float64, ctx.StreamBufferSize)
+	
+	go func() {
+		defer close(alphas)
+		
+		for freq := range freqs {
+			alphas <- ctx.HighPassCutoffToAlpha(freq)
+		}
+	}()
+	
+	return alphas
+}
+
 func (ctx Context) HighPass(input chan float64, alpha float64) (output chan float64) {
 	output = make(chan float64, ctx.StreamBufferSize)
 	
@@ -43,6 +72,31 @@ func (ctx Context) HighPassM(input chan float64, alphaModulation chan float64) (
 	}()
 	
 	return output
+}
+
+// Convert a low pass filter's cutoff frequency to an alpha value
+func (ctx Context) LowPassCutoffToAlpha(freq float64) (alpha float64) {
+	// Time interval between samples
+	dt := 1 / ctx.SampleRate
+	
+	// Time constant (of analogue circuit)
+	rc := 1 / (2 * math.Pi * freq)
+	
+	return dt / (rc + dt)
+}
+
+func (ctx Context) LowPassCutoffToAlphaM(freqs chan float64) (alphas chan float64) {
+	alphas = make(chan float64, ctx.StreamBufferSize)
+	
+	go func() {
+		defer close(alphas)
+		
+		for freq := range freqs {
+			alphas <- ctx.LowPassCutoffToAlpha(freq)
+		}
+	}()
+	
+	return alphas
 }
 
 func (ctx Context) LowPass(input chan float64, alpha float64) (output chan float64) {
