ac9e0b29d0a84a750456bf663a664a41f1503000
Kier Davis <kierdavis@gmail.com>
2014-04-25 01:06:54 +0100

    Add STFT algorithm

--- sound/fft/fft.go
+++ sound/fft/fft.go
@@ -7,44 +7,43 @@ import (
 
 // Return the smallest power of two >= x
 // Source: Section 3-2, Hacker's Delight (2nd Edition), Henry S. Warren, Jr.
-func clp2(x uint) uint {
-	x = x - 1
+func clp2(x uint64) uint64 {
+	x -= 1
 	x |= x >> 1
 	x |= x >> 2
 	x |= x >> 4
 	x |= x >> 8
 	x |= x >> 16
+	x |= x >> 32
 	return x + 1
 }
 
+// Fast Fourier transform using Cooley-Tukey algorithm.
 func FFT(input []float64) (output []complex128) {
-	inputCmplx := make([]complex128, clp2(uint(len(input))))
-	output = make([]complex128, clp2(uint(len(input))))
-	
-	for i, x := range input {
-		inputCmplx[i] = complex(x, 0)
-	}
-	
-	fft(output, inputCmplx, 1)
+	output = make([]complex128, clp2(uint64(len(input))))
+	fft(output, input, 1)
 	return output
 }
 
-func fft(output []complex128, input []complex128, stride int) {
+func fft(output []complex128, input []float64, stride int) {
 	n := len(output)
+	p := n / 2
 	
 	if n == 1 {
-		output[0] = input[0]
+		if len(input) > 0 {
+			output[0] = complex(input[0], 0)
+		} else {
+			output[0] = complex(0, 0)
+		}
 		return
 	}
 	
-	p := n / 2
 	fft(output[:p], input, stride * 2)
 	fft(output[p:], input[stride:], stride * 2)
 	
-	i := complex(0, 1)
 	for k, t := range output[:p] {
-		a := complex(-2 * math.Pi * float64(k) / float64(n), 0)
-		e := cmplx.Exp(a * i) * output[k + p]
+		a := complex(0, -2 * math.Pi * float64(k) / float64(n))
+		e := cmplx.Exp(a) * output[k + p]
 		output[k] = t + e
 		output[k + p] = t - e
 	}
---
+++ sound/fft/stft.go
@@ -0,0 +1,126 @@
+package fft
+
+import (
+	"math"
+)
+
+// Rolling short-time Fourier transform over an input channel.
+func STFT(input chan float64, window []float64, overlapSize int, canReuseResult bool) (output chan []float64) {
+	output = make(chan []float64)
+	
+	go func() {
+		defer close(output)
+		
+		var result []float64
+		
+		windowSize := len(window)
+		buffer := make([]float64, windowSize)
+		overlap := make([]float64, overlapSize)
+		pos := 0
+		
+		ok := true
+		for ok {
+			// Top up the buffer.
+			for pos < windowSize {
+				buffer[pos], ok = <-input
+				pos++
+			}
+			// Only check if the channel is closed after the end of the loop,
+			// since we would want to fill the rest of the buffer with zeroes
+			// and do one final FFT anyway.
+			// When ok is set to false, the main loop will end.
+			
+			// Save the overlap.
+			copy(overlap, buffer[windowSize-overlapSize:])
+			
+			// Apply the windowing function to the input.
+			for i := 0; i < windowSize; i++ {
+				buffer[i] *= window[i]
+			}
+			
+			// FFT it
+			cresult := FFT(buffer)
+			
+			// Find the squared magnitudes and put them into a result array.
+			if result == nil {
+				result = make([]float64, len(cresult))
+			}
+			for i, c := range cresult {
+				x, y := real(c), imag(c)
+				result[i] = x*x + y*y
+			}
+			
+			// Send the result
+			output <- result
+			
+			// If we can't reuse the result array, set it to nil.
+			if !canReuseResult {
+				result = nil
+			}
+			
+			// Copy the overlap to the start of the buffer.
+			copy(buffer, overlap)
+			pos = overlapSize
+		}
+	}()
+	
+	return output
+}
+
+func RectangularWindow(size int) (window []float64) {
+	window = make([]float64, size)
+	for i := 0; i < size; i++ {
+		window[i] = 1.0
+	}
+	return window
+}
+
+func TriangularWindow(size int) (window []float64) {
+	window = make([]float64, size)
+	p := (size - 1) / 2
+	for i := 0; i <= p; i++ {
+		window[i] = float64(i) / float64(p)
+	}
+	for i := p+1; i < size; i++ {
+		window[i] = 1.0 - float64(i - p) / float64(p)
+	}
+	return window
+}
+
+func WelchWindow(size int) (window []float64) {
+	window = make([]float64, size)
+	p := (size - 1) / 2
+	for i := 0; i < size; i++ {
+		a := float64(i - p) / float64(p + 1)
+		window[i] = 1.0 - a*a
+	}
+	return window
+}
+
+func CosineWindow(size int, a, b float64) (window []float64) {
+	window = make([]float64, size)
+	m := (math.Pi * 2.0) / (float64(size) - 1)
+	for i := 0; i < size; i++ {
+		window[i] = a - b * math.Cos(m * float64(i))
+	}
+	return window
+}
+
+func HanningWindow(size int) (window []float64) {
+	return CosineWindow(size, 0.5, 0.5)
+}
+
+func HammingWindow(size int) (window []float64) {
+	return CosineWindow(size, 0.54, 0.46)
+}
+
+// s is the standard deviation
+func GaussianWindow(size int, s float64) (window []float64) {
+	window = make([]float64, size)
+	p := float64(size - 1) / 2
+	for i := 0; i < size; i++ {
+		a := (float64(i) - p) / (s * p)
+		window[i] = math.Exp(-0.5 * a*a)
+	}
+	return window
+}
