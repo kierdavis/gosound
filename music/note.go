a69cbb967a6eb0656b25f2c313d6647f4b80785d
Kier Davis <kierdavis@gmail.com>
2014-04-25 20:06:24 +0100

    Add frequency-to-note conversion

--- music/note.go
+++ music/note.go
@@ -27,10 +27,49 @@ const (
 	BFlat = ASharp
 )
 
+func (letter NoteLetter) String() string {
+	switch letter {
+	case C:
+		return "C"
+	case CSharp:
+		return "C#"
+	case D:
+		return "D"
+	case DSharp:
+		return "D#"
+	case E:
+		return "E"
+	case F:
+		return "F"
+	case FSharp:
+		return "F#"
+	case G:
+		return "G"
+	case GSharp:
+		return "G#"
+	case A:
+		return "A"
+	case ASharp:
+		return "A#"
+	case B:
+		return "B"
+	}
+	
+	return ""
+}
+
 type Note int
 
 func MakeNote(letter NoteLetter, octave int) (note Note) {
 	return Note(octave * 12) + Note(letter)
+}
+
+func (note Note) Add(semitones int) (newNote Note) {
+	return note + Note(semitones)
+}
+
+func (note Note) Sub(other Note) (diff int) {
+	return int(note - other)
 }
 
 func (note Note) Letter() (letter NoteLetter) {
@@ -42,9 +81,13 @@ func (note Note) Octave() (octave int) {
 }
 
 func (note Note) Frequency() (freq float64) {
-	semitones := int(note - MakeNote(A, 4))
+	semitones := note.Sub(MakeNote(A, 4))
 	octaves := float64(semitones) / 12.0
 	return 440.0 * math.Pow(2.0, octaves)
 }
 
-
+func FromFrequency(freq float64) (note Note) {
+	octaves := math.Log2(freq / 440.0)
+	semitones := int(math.Floor(octaves * 12.0 + 0.5))
+	return MakeNote(A, 4).Add(semitones)
+}
