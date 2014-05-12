2038d7d4e5c7ae806e3c610f96c2f585ee02e4e4
Kier Davis <kierdavis@gmail.com>
2014-05-12 01:31:00 +0100

    Add phases back into waveform generation

--- sound/source.go
+++ sound/source.go
@@ -5,16 +5,16 @@ import (
 	"math/rand"
 )
 
-func (ctx Context) Saw(frequencyInput chan float64) (signalOutput chan float64) {
+func (ctx Context) SawWithPhase(frequencyInput chan float64, phase float64) (signalOutput chan float64) {
 	signalOutput = make(chan float64, ctx.StreamBufferSize)
 	
 	go func() {
 		defer close(signalOutput)
 		
-		x := 0.5
+		x := math.Mod(phase + 0.5, 1.0)
 		
 		for frequency := range frequencyInput {
-			signalOutput <- (x * 2.0) - 1.0
+			signalOutput <- x*2.0 - 1.0
 			x = math.Mod(x + (frequency / ctx.SampleRate), 1.0)
 		}
 	}()
@@ -22,26 +22,34 @@ func (ctx Context) Saw(frequencyInput chan float64) (signalOutput chan float64) 
 	return signalOutput
 }
 
-func (ctx Context) Triangle(frequencyInput chan float64) (signalOutput chan float64) {
+func (ctx Context) Saw(frequencyInput chan float64) (signalOutput chan float64) {
+	return ctx.SawWithPhase(frequencyInput, 0.0)
+}
+
+func (ctx Context) TriangleWithPhase(frequencyInput chan float64, phase float64) (signalOutput chan float64) {
 	signalOutput = make(chan float64, ctx.StreamBufferSize)
 	
-	saw := ctx.Saw(frequencyInput)
+	saw := ctx.SawWithPhase(frequencyInput, phase + 0.25)
 	
 	go func() {
 		defer close(signalOutput)
 		
 		for x := range saw {
-			signalOutput <- math.Abs(x)
+			signalOutput <- math.Abs(x)*2.0 - 1.0
 		}
 	}()
 	
 	return signalOutput
 }
 
-func (ctx Context) Square(frequencyInput chan float64, dutyInput chan float64) (signalOutput chan float64) {
+func (ctx Context) Triangle(frequencyInput chan float64) (signalOutput chan float64) {
+	return ctx.TriangleWithPhase(frequencyInput, 0.0)
+}
+
+func (ctx Context) SquareWithPhase(frequencyInput chan float64, dutyInput chan float64, phase float64) (signalOutput chan float64) {
 	signalOutput = make(chan float64, ctx.StreamBufferSize)
 	
-	saw := ctx.Saw(frequencyInput)
+	saw := ctx.SawWithPhase(frequencyInput, phase)
 	
 	go func() {
 		defer close(signalOutput)
@@ -59,10 +67,14 @@ func (ctx Context) Square(frequencyInput chan float64, dutyInput chan float64) (
 	return signalOutput
 }
 
-func (ctx Context) Sine(frequencyInput chan float64) (signalOutput chan float64) {
+func (ctx Context) Square(frequencyInput chan float64, dutyInput chan float64) (signalOutput chan float64) {
+	return ctx.SquareWithPhase(frequencyInput, dutyInput, 0.0)
+}
+
+func (ctx Context) SineWithPhase(frequencyInput chan float64, phase float64) (signalOutput chan float64) {
 	signalOutput = make(chan float64, ctx.StreamBufferSize)
 	
-	saw := ctx.Saw(frequencyInput)
+	saw := ctx.SawWithPhase(frequencyInput, phase)
 	
 	go func() {
 		defer close(signalOutput)
@@ -73,6 +85,10 @@ func (ctx Context) Sine(frequencyInput chan float64) (signalOutput chan float64)
 	}()
 	
 	return signalOutput
+}
+
+func (ctx Context) Sine(frequencyInput chan float64) (signalOutput chan float64) {
+	return ctx.SineWithPhase(frequencyInput, 0.0)
 }
 
 func (ctx Context) Silence() (output chan float64) {
