fb0a55d64f38aafc33401d7d3eda1b3ce7174ec3
Kier Davis <kierdavis@gmail.com>
2014-05-29 01:37:02 +0100

    Finish new filter interface

--- sound/filter/chebyshev.go
+++ sound/filter/chebyshev.go
@@ -5,154 +5,154 @@ import (
 	"math"
 )
 
-func Chebyshev(ctx sound.Context, input chan float64, filterType FilterType, cutoffFreq, percentRipple float64, numPoles int) (output chan float64) {
-	as, bs := ChebyshevCoefficients(ctx, filterType, cutoffFreq, percentRipple, numPoles)
+func Chebyshev(ctx sound.Context, input chan float64, filterType FilterType, cutoffFreqInput chan float64, percentRipple float64, numPoles int) (output chan float64) {
+	as, bs := ChebyshevCoefficients(ctx, filterType, cutoffFreqInput, percentRipple, numPoles)
 	return Recursive(ctx, input, as, bs)
 }
 
 // Based on http://www.dspguide.com/ch20/4.htm
-func ChebyshevCoefficients(ctx sound.Context, filterType FilterType, cutoffFreq, percentRipple float64, numPoles int) (as, bs []float64) {
-	// Must be a fraction of the sampling frequency
-	cutoffFreq /= ctx.SampleRate
+func ChebyshevCoefficients(ctx sound.Context, filterType FilterType, cutoffFreqInput chan float64, percentRipple float64, numPoles int) (asOutput, bsOutput []chan float64) {
+	n := numPoles+1
+	asOutput = make([]chan float64, n)
+	bsOutput = make([]chan float64, n)
+	asOutput[0] = make(chan float64, ctx.StreamBufferSize)
 	
-	// Calculate ellipse warp factors
-	var rpf, ipf float64
-	if percentRipple != 0 {
-		es := 100.0 / (100.0 - percentRipple)
-		es = math.Sqrt(es * es - 1.0)
-		vx := math.Log(1.0/es + math.Sqrt(1.0/(es*es) + 1.0)) / float64(numPoles)
-		kx := math.Log(1.0/es + math.Sqrt(1.0/(es*es) - 1.0)) / float64(numPoles)
-		kx = (math.Exp(kx) + math.Exp(-kx)) / 2.0
-		rpf = (math.Exp(vx) - math.Exp(-vx)) / (2.0 * kx)
-		ipf = (math.Exp(vx) + math.Exp(-vx)) / (2.0 * kx)
-	} else {
-		rpf = 1.0
-		ipf = 1.0
+	for i := 1; i < n; i++ {
+		asOutput[i] = make(chan float64, ctx.StreamBufferSize)
+		bsOutput[i] = make(chan float64, ctx.StreamBufferSize)
 	}
 	
-	t := 2.0 * math.Tan(0.5)
-	w := 2.0 * math.Pi * cutoffFreq
-	
-	// Not sure why this number exactly but I imagine it can be made variable
-	n := 20
-	as = make([]float64, n+2)
-	bs = make([]float64, n+2)
-	
-	as[2] = 1.0
-	bs[2] = 1.0
-	
-	// For each pole pair
-	for p := 0; p < numPoles/2; p++ {
-		
-		// Calculate pole location on unit circle
-		phase := math.Pi / (float64(numPoles) * 2.0) + (float64(p) * math.Pi / float64(numPoles))
-		rp, ip := -math.Cos(phase), math.Sin(phase)
-		
-		// Warp circle to an ellipse
-		rp *= rpf
-		ip *= ipf
-		
-		
-		// s-domain to z-domain conversion
-		m := rp*rp + ip*ip
-		d := 4.0 + t*(m*t - 4.0*rp)
-		x0 := (t*t) / d
-		x1 := (2.0*t*t) / d
-		x2 := (t*t) / d
-		y1 := (8.0 - 2.0*m*t*t) / d
-		y2 := (-4.0 - 4.0*rp*t - m*t*t) / d
-		
-		
-		// God knows
-		var k float64
+	go func() {
+		var s float64
 		switch filterType {
 		case LowPass:
-			k = math.Sin(0.5 - w/2) / math.Sin(0.5 + w/2)
+			s = 1.0
 		case HighPass:
-			k = -math.Cos(w/2 + 0.5) / math.Cos(w/2 - 0.5)
+			s = -1.0
 		}
 		
-		d = 1.0 + y1*k - y2*k*k
-		a0 := (x0 - x1*k + x2*k*k) / d
-		a1 := (-2.0*x0*k + x1 + x1*k*k - 2.0*x2*k) / d
-		a2 := (x0*k*k - x1*k + x2) / d
-		b1 := (2*k + y1 + y1*k*k - 2*y2*k) / d
-		b2 := (-k*k - y1*k + y2) / d
-		
-		if filterType == HighPass {
-			a1 = -a1
-			b1 = -b1
-		}
-		
-		
-		// Add coefficients to the cascade
-		ta2 := as[0]
-		ta1 := as[1]
-		tb2 := bs[0]
-		tb1 := bs[1]
-		
-		for i := 2; i < n+2; i++ {
-			ta0 := as[i]
-			tb0 := bs[i]
-			as[i] = a0*ta0 + a1*ta1 + a2*ta2
-			bs[i] = tb0 - b1*tb1 - b2*tb2
-			ta2, ta1 = ta1, ta0
-			tb2, tb1 = tb1, tb0
-		}
-	}
-	
-	// Finish combining coefficients
-	bs[2] = 0.0
-	for i := 0; i < n; i++ {
-		as[i] = as[i+2]
-		bs[i] = -bs[i+2]
-	}
-	as = as[:n]
-	bs = bs[:n]
-	
-	// Normalise the gain
-	sa := 0.0
-	sb := 0.0
-	
-	switch filterType {
-	case LowPass:
-		for i := 0; i < n; i++ {
-			sa += as[i]
-			sb += bs[i]
-		}
-	
-	case HighPass:
-		m := 1.0
-		for i := 0; i < n; i++ {
-			sa += as[i] * m
-			sb += bs[i] * m
-			m = -m
-		}
-	}
-	
-	gain := sa / (1 - sb)
-	for i := 0; i < n; i++ {
-		as[i] /= gain
-	}
-	
-	// Trim trailing zeroes.
-	for i := n-1; i >= 0; i-- {
-		if as[i] == 0.0 {
-			as = as[:i]
+		// Calculate ellipse warp factors
+		var rpf, ipf float64
+		if percentRipple != 0 {
+			es := 100.0 / (100.0 - percentRipple)
+			es = math.Sqrt(es * es - 1.0)
+			vx := math.Log(1.0/es + math.Sqrt(1.0/(es*es) + 1.0)) / float64(numPoles)
+			kx := math.Log(1.0/es + math.Sqrt(1.0/(es*es) - 1.0)) / float64(numPoles)
+			kx = (math.Exp(kx) + math.Exp(-kx)) / 2.0
+			rpf = (math.Exp(vx) - math.Exp(-vx)) / (2.0 * kx)
+			ipf = (math.Exp(vx) + math.Exp(-vx)) / (2.0 * kx)
 		} else {
-			break
+			rpf = 1.0
+			ipf = 1.0
 		}
-	}
-	for i := n-1; i >= 0; i-- {
-		if bs[i] == 0.0 {
-			bs = bs[:i]
-		} else {
-			break
+		
+		t := 2.0 * math.Tan(0.5)
+		tt := t * t
+		
+		as := make([]float64, n)
+		bs := make([]float64, n)
+		
+		x0s := make([]float64, numPoles/2)
+		x1s := make([]float64, numPoles/2)
+		x2s := make([]float64, numPoles/2)
+		y1s := make([]float64, numPoles/2)
+		y2s := make([]float64, numPoles/2)
+		
+		// For each pole pair
+		for p := 0; p < numPoles/2; p++ {
+			// Calculate pole location on unit circle
+			phase := math.Pi / (float64(numPoles) * 2.0) + (float64(p) * math.Pi / float64(numPoles))
+			rp, ip := -math.Cos(phase), math.Sin(phase)
+			
+			// Warp circle to an ellipse
+			rp *= rpf
+			ip *= ipf
+			
+			// s-domain to z-domain conversion
+			mtt := (rp*rp + ip*ip) * tt
+			rpt := rp * t
+			d := 4.0 + mtt - 4.0*rpt
+			x0s[p] = tt / d
+			x1s[p] = (2.0*tt) / d
+			x2s[p] = tt / d
+			y1s[p] = (8.0 - 2.0*mtt) / d
+			y2s[p] = (-4.0 - 4.0*rpt - mtt) / d
 		}
-	}
+		
+		for cutoffFreq := range cutoffFreqInput {
+			w := 2.0 * math.Pi * (cutoffFreq / ctx.SampleRate)
+			
+			as[0] = 1.0
+			bs[0] = -1.0
+			for i := 1; i < n; i++ {
+				as[i] = 0.0
+				bs[i] = 0.0
+			}
+			
+			for p := 0; p < numPoles/2; p++ {
+				var k float64
+				switch filterType {
+				case LowPass:
+					k = math.Sin(0.5 - w/2) / math.Sin(0.5 + w/2)
+				case HighPass:
+					k = -math.Cos(w/2 + 0.5) / math.Cos(w/2 - 0.5)
+				}
+				
+				x0 := x0s[p]
+				x1 := x1s[p]
+				x2 := x2s[p]
+				y1 := y1s[p]
+				y2 := y2s[p]
+				
+				d := 1.0 + (y1 - y2*k)*k
+				a0 := (x0 + (x2*k - x1)*k) / d
+				a1 := ((x1*k - 2.0*(x0 + x2))*k + x1) / d
+				a2 := ((x0*k - x1)*k + x2) / d
+				b1 := ((2.0 - 2.0*y2 + y1*k)*k + y1) / d
+				b2 := (y2 - (k + y1)*k) / d
+				
+				a1 *= s
+				b1 *= s
+				
+				// Add coefficients to the cascade
+				ta2 := 0.0
+				ta1 := 0.0
+				tb2 := 0.0
+				tb1 := 0.0
+				
+				for i := 0; i < n; i++ {
+					ta0 := as[i]
+					tb0 := -bs[i]
+					as[i] = a0*ta0 + a1*ta1 + a2*ta2
+					bs[i] = -tb0 + b1*tb1 + b2*tb2
+					ta2, ta1 = ta1, ta0
+					tb2, tb1 = tb1, tb0
+				}
+			}
+			
+			// Finish combining coefficients
+			bs[0] = 0.0
+			
+			// Normalise the gain
+			sa := 0.0
+			sb := 0.0
+			
+			m := 1.0
+			for i := 0; i < n; i++ {
+				sa += as[i] * m
+				sb += bs[i] * m
+				m *= s
+			}
+			
+			gain := sa / (1 - sb)
+			for i := 0; i < n; i++ {
+				asOutput[i] <- as[i] / gain
+				if i != 0 {
+					bsOutput[i] <- bs[i]
+				}
+			}
+		}
+	}()
 	
-	// Trim the unused b0 coefficient.
-	bs = bs[1:]
-	
-	return as, bs
+	return asOutput, bsOutput
 }
--- sound/filter/recursive.go
+++ sound/filter/recursive.go
@@ -5,43 +5,37 @@ import (
 )
 
 // Run a recursive filter using input coefficients 'as' and past output
-// coefficients 'bs'.
+// coefficients 'bs'. 'bs[0]' is ignored.
 func Recursive(ctx sound.Context, input chan float64, as, bs []chan float64) (output chan float64) {
-    output = make(chan float64, ctx.StreamBufferSize)
-    
-    go func() {
-        defer close(output)
-        
-        a0 := ctx.Const(0.0)
-        if len(as) >= 1 {
-            a0 = as[0]
-            as = as[1:]
-        }
-        
-        prevInputs := make([]float64, len(as))
-        prevOutputs := make([]float64, len(bs))
-        prevInputPtr := 0 // points to most recently added prevInput
-        prevOutputPtr := 0 // points to most recently added prevOutput
-        
-        for x := range input {
-            y := (<-a0) * x
-            
-            for i, a := range as {
-                y += (<-a) * prevInputs[(prevInputPtr - i + len(prevInputs)) % len(prevInputs)]
-            }
-            
-            for i, b := range bs {
-                y += (<-b) * prevOutputs[(prevOutputPtr - i + len(prevOutputs)) % len(prevOutputs)]
-            }
-            
-            output <- y
-           	
-            prevInputPtr = (prevInputPtr + 1) % len(prevInputs)
-            prevInputs[prevInputPtr] = x
-            prevOutputPtr = (prevOutputPtr + 1) % len(prevOutputs)
-            prevOutputs[prevOutputPtr] = y
-        }
-    }()
-    
-    return output
+	output = make(chan float64, ctx.StreamBufferSize)
+	
+	go func() {
+		defer close(output)
+		
+		prevInputs := make([]float64, len(as))
+		prevOutputs := make([]float64, len(bs))
+		prevInputPtr := 0 // points to most recently added prevInput
+		prevOutputPtr := 0 // points to most recently added prevOutput
+		
+		for x := range input {
+			y := (<-as[0]) * x
+			
+			for i, a := range as[1:] {
+				y += (<-a) * prevInputs[(prevInputPtr - i + len(prevInputs)) % len(prevInputs)]
+			}
+			
+			for i, b := range bs[1:] {
+				y += (<-b) * prevOutputs[(prevOutputPtr - i + len(prevOutputs)) % len(prevOutputs)]
+			}
+			
+			output <- y
+			
+			prevInputPtr = (prevInputPtr + 1) % len(prevInputs)
+			prevInputs[prevInputPtr] = x
+			prevOutputPtr = (prevOutputPtr + 1) % len(prevOutputs)
+			prevOutputs[prevOutputPtr] = y
+		}
+	}()
+	
+	return output
 }
